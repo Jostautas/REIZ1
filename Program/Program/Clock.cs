6e72d742fe435093129b4afa0d19d3389e14e6a2
Jostautas <jostautas.sakas@gmail.com>
2022-11-28 09:55:12 +0200

    Angle calculation done

--- Program/Program/Clock.cs
+++ Program/Program/Clock.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Program;
+﻿namespace Program;
 
 public class Clock
 {
@@ -13,6 +11,8 @@ public class Clock
 
     public void GetInput()
     {
+        Console.WriteLine("Valid input: 0 <= Hours <= 12 | 0 <= Minutes <= 720");
+
         Console.WriteLine("Enter Hours:");
         Hours = Convert.ToInt32(Console.ReadLine());
 
@@ -27,7 +27,40 @@ public class Clock
 
     public double GetAngle()
     {
+        int hourAngle = GetHourAngle();
+        double minuteAngle = GetMinuteAngle();
 
-        return -1;
+        double angle = Math.Abs(hourAngle - minuteAngle);
+
+        if(angle > 180)
+        {
+            angle = 360 - angle;
+        }
+
+        return angle;
+    }
+
+    private int GetHourAngle()
+    {
+        if(Hours == 0 || Hours == 12)
+        {
+            return 0;
+        }
+        else
+        {
+            return Hours * 30;      // 360 / 12 = 30
+        }
+    }
+
+    private double GetMinuteAngle()
+    {
+        if (Minutes == 0 || Minutes == 720)
+        {
+            return 0;
+        }
+        else
+        {
+            return Minutes * 0.5;      // 30 / 60 = 1/2
+        }
     }
 }
