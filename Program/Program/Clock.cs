0dbca3954fe9281e1af6c6c9eb92352ea3b5aae3
Jostautas <jostautas.sakas@gmail.com>
2022-11-28 09:25:41 +0200

    Clock App Started

---
+++ Program/Program/Clock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Program;
+
+public class Clock
+{
+    private int Hours { get; set; }
+    private int Minutes { get; set; }
+
+    public Clock()
+    {
+    }
+
+    public void GetInput()
+    {
+        Console.WriteLine("Enter Hours:");
+        Hours = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter Minutes:");
+        Minutes = Convert.ToInt32(Console.ReadLine());
+    }
+
+    public void PrintTime()
+    {
+        Console.WriteLine("Entered time: " + Hours + ":" + Minutes);
+    }
+
+    public double GetAngle()
+    {
+
+        return -1;
+    }
+}
---
+++ Program/Program/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Program;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Clock Angle App");
+
+        Clock c = new();
+        c.GetInput();
+        c.PrintTime();
+
+        double angle = c.GetAngle();
+        if(angle == -1)
+        {
+            Console.WriteLine("ERROR: unable to calculate angle");
+        }
+        else
+        {
+            Console.WriteLine("The angle of clock arms is " + angle + " degrees");
+        }
+    }
+}
